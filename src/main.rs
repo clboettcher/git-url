c98f178699c8fcbea3388c7ff306d5a77e008cc5
Claudius Boettcher <claudius.boettcher@qaware.de>
2020-05-14 16:33:37 +0200

    Print origin url, forward errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// gh='xdg-open ${$(git remote get-url origin)//.git/}'
+
+use std::process::{Command, exit};
+
+// Not in git repo -> print err and exit
+// No arguments -> result of git remote get-url origin
+
 fn main() {
-    println!("Hello, world!");
+    let out = Command::new("git")
+        .args(&["remote", "get-url", "origin"])
+        .output()
+        .expect("git command failed");
+
+    // println!("status: {}", out.status);
+    // println!("stdout: {}", String::from_utf8_lossy(&out.stdout));
+    // println!("stderr: {}", String::from_utf8_lossy(&out.stderr));
+    if !out.status.success() {
+        eprint!("{} {}", "Could not get url:", String::from_utf8_lossy(&out.stderr));
+        exit(out.status.code().unwrap_or(1));
+    }
+
+    println!("{}", String::from_utf8_lossy(&out.stdout).trim_end().to_owned());
 }
